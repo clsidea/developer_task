a193c6760a05a50cfad405131af1843006987ae0
Hizbucodes <hizbusoftware@gmail.com>
2026-02-11 16:09:07 +0530

    initial commit

---
+++ TaskManagement.API/TaskManagement/Models/Enums/TaskStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManagement.Models.Enums
+{
+    public enum TaskStatus
+    {  
+        Pending = 0,
+        Completed = 1
+    }
+}
---
+++ TaskManagement.API/TaskManagement/Models/TaskItem.cs
@@ -0,0 +1,27 @@
+﻿using TaskManagement.Models.Enums;
+
+namespace TaskManagement.Models
+{
+    public class TaskItem
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        public Enums.TaskStatus Status { get; set; } = Enums.TaskStatus.Pending;
+
+        public DateTime? DueDate { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+
+        public string UserId { get; set; } = null!; 
+
+  
+        public User User { get; set; } = null!;
+
+        public ICollection<TaskEvent> TaskEvents { get; set; } = new List<TaskEvent>();
+    }
+}
